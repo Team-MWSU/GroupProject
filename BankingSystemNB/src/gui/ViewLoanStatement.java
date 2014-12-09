8f68628794b51fb0fd21e7f93d0bb5619699d024
rstith <rstith@missouriwestern.edu>
2014-12-08 21:27:52 -0600

    modified ViewLoanStatement event

--- BankingSystemNB/src/gui/ViewLoanStatement.java
+++ BankingSystemNB/src/gui/ViewLoanStatement.java
@@ -72,7 +72,6 @@ public class ViewLoanStatement extends javax.swing.JFrame {
             }
         });
 
-        jTextField1.setText("jTextField1");
         jTextField1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jTextField1ActionPerformed(evt);
